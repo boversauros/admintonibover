ecf927fd9e94d8243f569020672b7e87a66f805e
Oriol Bover Vila <bover73@gmail.com>
2025-04-06 13:29:50 +0200

    add back React for React.fragment

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import React, { useState } from "react";
 import { DEMO_POSTS, CATEGORIES } from "../data";
 import { Post } from "../types";
 import { PlusCircle, Edit, Trash, Eye, EyeOff } from "lucide-react";
