17335c93fb6fff79efb6fac00ead4a9bfea7cda5
Oriol Bover Vila <bover73@gmail.com>
2025-12-27 15:06:18 +0100

    fix: update CSS class syntax for improved styling consistency
    - Corrected font-family syntax in the main container.
    - Adjusted background color classes for better compatibility.
    - Refined border color classes for enhanced visual clarity.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-(family-name:--font-geist-sans)">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1 className="text-4xl font-bold text-center sm:text-left">
           Welcome to Next.js!
         </h1>
         <p className="text-center sm:text-left text-sm sm:text-base">
           Get started by editing{" "}
-          <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+          <code className="bg-black/5 dark:bg-white/6 px-1 py-0.5 rounded font-semibold">
             app/page.tsx
           </code>
         </p>
@@ -21,7 +21,7 @@ export default function Home() {
             Read our docs
           </a>
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            className="rounded-full border border-solid border-black/8 dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
             href="https://vercel.com/new"
             target="_blank"
             rel="noopener noreferrer"
